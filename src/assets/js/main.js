97f6ff614080e70851c83d7fff12fe8b9e0bb721
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-02-22 13:11:40 +0000

    initial commit for embargo

---
+++ src/assets/js/main.js
@@ -0,0 +1,37 @@
+
+/**
+ * Ensphere Container
+ */
+$.fn.ensphere = new function() {
+
+    /**
+     *
+     * @type {embargo}
+     */
+    this.embargo = new function() {
+
+        var removePageLoader = function()
+        {
+            var loader = $('#loader');
+            if( loader.length ) {
+                loader.fadeOut( 200, function(){
+                    $(this).remove();
+                });
+            }
+        };
+
+        var onDocumentReady = function()
+        {
+
+        };
+
+        var onWindowLoad = function()
+        {
+            removePageLoader();
+        };
+
+        $(document).ready( onDocumentReady );
+        $(window).load( onWindowLoad );
+
+    };
+};
