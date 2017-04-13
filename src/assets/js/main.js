2bd1280e5a33e515bc70c4b1d61eac186c4d5fc4
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-04-13 08:57:34 +0100

    added api renderer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -20,9 +20,29 @@ $.fn.ensphere = new function() {
             }
         };
 
+        var distributeApiRequests = function()
+        {
+            var methods = [];
+            $('[data-api]').each( function(){
+                methods.push({
+                    method : $(this).attr( 'data-api' ),
+                    callback : $(this).attr( 'data-api-callback' )
+                });
+            });
+            $.get( '/api/render', { actions : methods }, function( responses ) {
+                for( var i in responses ) {
+                    if( typeof window[responses[i].callback] !== 'undefined' ) {
+                        window[responses[i].callback](responses[i].response);
+                    } else {
+                        console.log( '[API Renderer] ' + responses[i].callback + ' is not a valid api callback' );
+                    }
+                }
+            });
+        };
+
         var onDocumentReady = function()
         {
-
+            distributeApiRequests();
         };
 
         var onWindowLoad = function()
