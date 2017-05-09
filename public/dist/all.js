46547891c9dc620691cea2bbefd7cc2739a658b7
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-05-09 14:33:14 +0100

    update to api request

--- public/dist/all.js
+++ public/dist/all.js
@@ -10,6 +10,27 @@ $.fn.ensphere = new function() {
      */
     this.embargo = new function() {
 
+        /**
+         * GUID
+         * @returns {string}
+         */
+        var guid = function()
+        {
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+        }
+
+        /**
+         * S4
+         * @returns {string}
+         */
+        var s4 = function()
+        {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+
+        /**
+         * Remove Page Loader
+         */
         var removePageLoader = function()
         {
             var loader = $('#loader');
@@ -24,15 +45,19 @@ $.fn.ensphere = new function() {
         {
             var methods = [];
             $('[data-api]').each( function(){
+                var _guid = guid();
+                $(this).attr( 'data-guid', _guid );
                 methods.push({
                     method : $(this).attr( 'data-api' ),
-                    callback : $(this).attr( 'data-api-callback' )
+                    callback : $(this).attr( 'data-api-callback' ),
+                    modelId : parseInt( $(this).attr( 'data-model-id' ) ),
+                    guid : _guid
                 });
             });
             $.get( '/api/render', { actions : methods }, function( responses ) {
                 for( var i in responses ) {
                     if( typeof window[responses[i].callback] !== 'undefined' ) {
-                        window[responses[i].callback](responses[i].response);
+                        window[responses[i].callback](responses[i].response, $('[data-guid="' + responses[i].guid + '"]') );
                     } else {
                         console.log( '[API Renderer] ' + responses[i].callback + ' is not a valid api callback' );
                     }
